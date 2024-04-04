e7e82b80ac51a6c02695631d012b23792a0f4d87
Malcolm <146789335+Malcolm-LaRose@users.noreply.github.com>
2024-04-04 18:48:57 -0400

    better

--- SDLEventDetection.cpp
+++ SDLEventDetection.cpp
@@ -9,15 +9,19 @@ void quitSDL(SDL_Renderer* rend, SDL_Window* wind) {
     exit(0);
 }
 
+
+
 int main(int argc, char* argv[]) {
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL initialization failed: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("SDL Mouse Motion Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 3840, 2160, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_HIDDEN);
+    SDL_Window* window = SDL_CreateWindow("SDL Mouse Motion Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 3840, 2160, SDL_WINDOW_FULLSCREEN);
     SDL_HideWindow(window);
-    SDL_SetWindowOpacity(window, 0.05f);
+    SDL_SetWindowOpacity(window, 0.25f);
+
     if (!window) {
         printf("Failed to create SDL window: %s\n", SDL_GetError());
         SDL_Quit();
@@ -25,6 +29,7 @@ int main(int argc, char* argv[]) {
     }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
     if (!renderer) {
         printf("Failed to create renderer: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
@@ -34,38 +39,36 @@ int main(int argc, char* argv[]) {
 
     bool quit = false;
     while (!quit) {
-        bool validEventDetected = false;
-        while (!validEventDetected) {
-            SDL_Event test_event;
-            while (SDL_PollEvent(&test_event)) {
 
-                auto& pressedKey = test_event.key.keysym.sym;
-                
-                    if (test_event.type == SDL_QUIT) {
-                        quit = true;
-                        break;
-                    }
-                    
-                    if (test_event.type == SDL_MOUSEMOTION) {
-                        validEventDetected = true;
-                        printf("Current mouse position is: (%d, %d)\n", test_event.motion.x, test_event.motion.y);
-                        break;
-                    }
-                    if (test_event.type == SDL_KEYDOWN && test_event.key.repeat == 0) {
-                        validEventDetected = true;
-                        printf("This key was pressed: %c \n", *SDL_GetKeyName(pressedKey));
-                        // Compare pressed key to appropriate key in displayed strategem
-                        if (pressedKey == SDLK_DELETE) {
-                            quit = true;
-                            quitSDL(renderer, window);
-                            break;
-                        }
-                    }
-                    else {
-                        // Do nothing
-                        // printf("Unhandled Event!\n");
-                    }
+        SDL_Event test_event;
+        while (SDL_PollEvent(&test_event)) {
+
+            auto& pressedKey = test_event.key.keysym.sym;
+
+            if (test_event.type == SDL_QUIT) {
+                quit = true;
+                break;
             }
+
+            if (test_event.type == SDL_MOUSEMOTION) {
+                printf("Current mouse position is: (%d, %d)\n", test_event.motion.x, test_event.motion.y);
+                break;
+            }
+            if (test_event.type == SDL_KEYDOWN && test_event.key.repeat == 0) {
+                printf("This key was pressed: %c\n", *SDL_GetKeyName(pressedKey));
+                // Compare pressed key to appropriate key in displayed strategem
+                if (pressedKey == SDLK_DELETE) {
+                    printf("Quitting...\n");
+                    quit = true;
+                    quitSDL(renderer, window);
+                    break;
+                }
+            }
+            else {
+                // Do nothing
+                // printf("Unhandled Event!\n");
+            }
+
         }
     }
 
