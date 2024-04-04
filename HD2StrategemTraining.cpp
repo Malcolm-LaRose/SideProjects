5af8b8bd9611fbaba675d10232c5080325098969
Malcolm <146789335+Malcolm-LaRose@users.noreply.github.com>
2024-04-03 21:39:07 -0400

    more work

--- HD2StrategemTraining.cpp
+++ HD2StrategemTraining.cpp
@@ -1,3 +1,5 @@
+#include <SDL.h>
+
 #include <string>
 #include <vector>
 #include <list>
@@ -34,8 +36,13 @@ private:
 
 };
 
-class Game {
+class StrategemList {
 public:
+
+	// Constructor
+	StrategemList() : strategems() {
+		initStrategems();
+	}
 
 	void displayStrategem(Strategem strat) {
 		std::cout << strat.getName() << ": ";
@@ -45,28 +52,104 @@ public:
 		std::cout << std::endl;
 	}
 
+	void displayAllStrats() {
+		for (Strategem strat : strategems) {
+			std::cout << strat.getName() << ": ";
+			for (char& key : strat.getKeys()) {
+				std::cout << key << ' ';
+			}
+			std::cout << std::endl;
+		}
+
+	}
+
 	void addStrategem(Strategem strat) {
 		strategems.push_back(strat);
+	}
+
+	void initStrategems() {
+		// Declare and initialize strategems
+		Strategem orbitalGatling("Orbital Gatling Barrage", { 'd','s','a','w','w' });
+		Strategem orbitalAirburst("Orbital Airburst Strike", { 'd','d','d' });
+		Strategem orbital120("Orbital 120MM Barrage", { 'd','d','s','a','d','s' });
+		Strategem orbital380("Orbital 380MM Barrage", { 'd','s','w','w','a','s','s' });
+		Strategem orbitalWalking("Orbital Walking Barrage", { 'd','s','d','s','d','s' });
+		Strategem orbitalLaser("Orbital Laser", { 'd','s','w','d','s' });
+		Strategem orbitalRail("Orbital Railcannon Strike", { 'd','w','s','s','d' });
+		Strategem orbitalStrike("Orbital Precision Strike", { 'd','d','w' });
+		Strategem orbitalGas("Orbital Gas Strike", { 'd','d','s','d' });
+		Strategem orbitalEMS("Orbital EMS Strike", { 'd','d','a','s' });
+		Strategem orbitalSmoke("Orbital Smoke Strike", { 'd','d','s','w' });
+
+		// Load them into strategems list
+		addStrategem(orbitalGatling);
+		addStrategem(orbitalAirburst);
+		addStrategem(orbital120);
+		addStrategem(orbital380);
+		addStrategem(orbitalWalking);
+		addStrategem(orbitalLaser);
+		addStrategem(orbitalRail);
+		addStrategem(orbitalStrike);
+		addStrategem(orbitalGas);
+		addStrategem(orbitalEMS);
+		addStrategem(orbitalSmoke);
+
+
 	}
 
 
 private:
 	std::list<Strategem> strategems;
 
+};
+
+class Menu {}; // Menu for the game, need to include options (for keybindings) and list of all strats (should also include credits) (bonus for saving stats somehow)
+
+class Game {
+public:
+
+	Strategem chooseRandomStrategem() {
+	
+	}
+
+	void handleInput() {
+		// Look for keypresses
+		// Ignore anything invalid
+		// Maybe encapsulate in separate class (later)
+	}
+
+	void compareInputToStrategem() {
+		// Display random strategem
+		// Handle user input
+		// Compare input to strategem[0]
+		// If correct, continue to loop, otherwise restart
+			// Figure out how to show correct input and incorrect input leading to reset
+	}
+
+	void stats() {
+		// Get average time to complete 
+		// Get % correct
+	}
+
+	void gameLoop() {
+		// Skeleton
+		const Strategem chosenStrategem = chooseRandomStrategem();
+		while (true) {
+			handleInput();
+			compareInputToStrategem();
+		}
+	}
+
 
 };
 
 
 
+int main(int argc, char* argv[]) {
 
-int main() {
+	StrategemList sL;
 
-	Strategem orbitalStrike("Orbital Strike", { 'd','d','s' });
-
-	Game game;
-
-	orbitalStrike.displayStrategem();
-	
+	sL.displayAllStrats();
 
 	return 0;
 }
