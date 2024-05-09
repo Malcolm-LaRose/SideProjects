28b6b61bb89ff948e4fd4823d976d4d02cf35bc5
Malcolm <146789335+Malcolm-LaRose@users.noreply.github.com>
2024-05-09 12:51:34 -0400

    test
    not sure if better than non test version, doesnt seem to be tho

---
+++ GeoRenderingTwoTest.cpp
@@ -0,0 +1,207 @@
+#include "Shapes.h"
+#include "Color.h"
+#include "Timer.h"
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+
+#include <chrono>
+#include <sstream> 
+#include <string> // Inherited from sstream
+#include <iostream>
+#include <thread>
+#include <stdio.h> // Inherited from SDL.h
+
+#include <windows.h>
+
+struct MySettings {
+
+	static MySettings& getInstance() {
+		static MySettings instance;
+		return instance; // Returns a static reference to the MySettings struct
+	}
+
+	const int initScreenWidth = 1920;
+	const int initScreenHeight = 1080;
+
+	const std::string windowTitle = "MyWindow";
+
+	const SDL_Color bgColor = Color::getSDLColor(Color::EIGENGRAU);
+
+	const int frameRateCap = 60;
+
+
+};
+
+class MyObject {};
+
+class MySDLWrapper { // Should be turned into GameX or SimX when implemented
+public:
+
+	MySDLWrapper() : screenWidth(settings.initScreenWidth), screenHeight(settings.initScreenHeight) {
+		init(); // Automatically runs init when instantiating MySDLWrapper
+	}
+
+
+	void init() {
+		running = true;
+	
+		// Initialize SDL
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+			running = false;
+		}
+		else
+		{
+
+			// Create window
+			window = SDL_CreateWindow("SDL Playground", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+			if (window == NULL)
+			{
+				printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+				running = false;
+			}
+			else
+			{
+				// Create renderer for window
+				renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+				if (renderer == NULL)
+				{
+					printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+					running = false;
+				}
+
+				// Initialize SDL_ttf
+				else if (TTF_Init() != 0) {
+					printf("SDL_ttf initialization failed");
+					running = false;
+				}
+
+				// Init background
+				SDL_SetRenderDrawColor(renderer, settings.bgColor.r, settings.bgColor.g, settings.bgColor.b, settings.bgColor.a);
+
+				// Init the timer
+				timer = new MyTimer_us; 
+
+			}
+		}
+
+		if (!running)
+		{
+			SDL_Quit();
+			renderer = nullptr;
+			window = nullptr;
+		}
+
+	}
+
+	void handleEvents() {
+		timer->markFrameBeginTime();
+		SDL_Event event;
+		SDL_PollEvent(&event); // Pointer to event
+
+		switch (event.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+
+			// Add cases
+
+			default:
+				break;
+		
+		}
+	
+	}
+
+	void update() {
+		count++; // Game frame counter
+		// std::cout << count << std::endl;
+
+
+	
+	}
+
+	void render() {
+		SDL_RenderClear(renderer);
+
+		// Add stuff to render
+	
+
+		SDL_RenderPresent(renderer);
+
+		// capFPS(settings.frameRateCap, MyTimer_us::getFrameDuration());
+		timer->markFrameEndTime();
+		timer->logFPS();
+	}
+
+	void clean() {
+	
+		SDL_DestroyWindow(window);
+		SDL_DestroyRenderer(renderer);
+		SDL_Quit();
+		timer = nullptr;
+		std::cout << "Cleanup complete!";
+
+	}
+
+	bool isRunning() {
+		return running;
+	}
+
+
+
+
+private:
+	const MySettings& settings = MySettings::getInstance();
+
+	bool running = false;
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+
+	int screenWidth; // Can change with user input
+	int screenHeight;
+
+	uint64_t count = 0;
+
+	MyTimer_us* timer = nullptr;
+
+
+	// Internal member functions
+
+
+
+
+};
+
+
+
+
+
+int main(int argc, char* argv[]) {
+
+	// Object Instantiation
+
+	MySDLWrapper* wrapper = nullptr; // Wrapper points toward the MySDLWrapper class
+
+	wrapper = new MySDLWrapper;
+
+
+	// Main loop
+	while (wrapper->isRunning()) {
+
+	
+		wrapper->handleEvents(); // Event handling
+		wrapper->update(); // Game logic
+		wrapper->render(); // Rendering
+
+	}
+
+	// Quit
+	wrapper->clean();
+	wrapper = nullptr;
+
+	return 0;
+}
