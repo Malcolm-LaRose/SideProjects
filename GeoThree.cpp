b3ad280c2077c17983f269c7f630967bc37c5700
Malcolm <146789335+Malcolm-LaRose@users.noreply.github.com>
2024-05-13 11:44:43 -0400

    update

--- GeoThree.cpp
+++ GeoThree.cpp
@@ -1,6 +1,6 @@
-// #include "Shapes.h"
-#include "color.h"
+#include "Color.h"
 #include "Timer.h" // USING SDL UNTIL THIS IS WORKING PROPERLY
+#include "textureManager.h"
 
 #include <SDL.h>
 #include <SDL_ttf.h>
@@ -15,6 +15,11 @@
 
 #include <windows.h>
 
+
+// Conventions
+// O_classMember --> Unique name for an object class member
+
+
 struct MySettings {
 
 	static MySettings& getInstance() {
@@ -22,26 +27,85 @@ struct MySettings {
 		return instance; // Returns a static reference to the MySettings struct
 	}
 
-	const int initScreenWidth = 1920.0;
-	const int initScreenHeight = 1080.0;
+	const float initScreenWidth = 1920.0;
+	const float initScreenHeight = 1080.0;
 
-	const SDL_Point screenCenter = { initScreenWidth / 2 , initScreenHeight / 2 };
-	
+	const SDL_FPoint screenCenter = { initScreenWidth / 2 , initScreenHeight / 2 };
+
 	const std::string windowTitle = "MyWindow";
 
 	const SDL_Color bgColor = Color::getSDLColor(Color::EIGENGRAU);
 
-	const int frameRateCap = 600;
+	const int frameRateCap = 60;
 
 
 
 
 };
 
-struct myRectangle {
+class GameObject {
+public:
+	GameObject(const char* texturePath, SDL_Renderer* renderer, float x, float y) {
+
+		O_renderer = renderer;
+		O_texture = TextureManager::LoadTexture(texturePath, renderer);
+
+		xPos = x;
+		yPos = y;
+
+	}
+	~GameObject();
 
 
+	void update() {
 
+		xPos++;
+		yPos++;
+
+		srcRect.h = 200;
+		srcRect.w = 200;
+		srcRect.x = 0;
+		srcRect.y = 0;
+
+		destRect.x = xPos;
+		destRect.y = yPos;
+		destRect.w = srcRect.w * 2;
+		destRect.h = srcRect.h * 2;
+
+
+	}
+	void renderTexture() {
+
+		SDL_RenderCopyF(O_renderer, O_texture, &srcRect, &destRect);
+
+	}
+
+	void renderToTexture() {
+
+		SDL_Rect rect = { settings.screenCenter.x, settings.screenCenter.y, 200, 200 };
+		SDL_SetRenderDrawColor(O_renderer, settings.bgColor.r, settings.bgColor.g, settings.bgColor.b, settings.bgColor.a);
+		SDL_RenderFillRect(O_renderer, &rect);
+
+		O_texture = SDL_CreateTexture(O_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 200, 200);
+
+		SDL_SetRenderTarget(O_renderer, O_texture);
+		SDL_RenderCopy(O_renderer, O_texture, nullptr, nullptr);
+		SDL_SetRenderTarget(O_renderer, nullptr);
+
+	}
+
+
+private:
+
+	float xPos;
+	float yPos;
+
+	SDL_Texture* O_texture;
+	SDL_Rect srcRect;
+	SDL_FRect destRect;
+	SDL_Renderer* O_renderer;
+
+	const MySettings& settings = MySettings::getInstance();
 
 
 };
@@ -108,6 +172,8 @@ public:
 			window = nullptr;
 		}
 
+		testPlayer = new GameObject("Untitled.png", renderer, 0, 0);
+
 	}
 
 	void handleEvents() {
@@ -132,7 +198,7 @@ public:
 		// Perform other updates here as needed
 
 
-
+		testPlayer->update();
 
 		count++;
 	}
@@ -143,7 +209,7 @@ public:
 		renderBG();
 
 		// Add stuff to render
-
+		testPlayer->renderTexture();
 		SDL_RenderPresent(renderer);
 
 		// capFPS(settings.frameRateCap, MyTimer_us::getFrameDuration());
@@ -167,7 +233,7 @@ public:
 	}
 
 
-	uint64_t count = 0;
+
 
 private:
 	const MySettings& settings = MySettings::getInstance();
@@ -176,9 +242,13 @@ private:
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 
+	SDL_Texture* testText;
+	GameObject* testPlayer;
 
 	int screenWidth; // Can change with user input
 	int screenHeight;
+
+	uint64_t count = 0;
 
 
 	// Internal member functions
@@ -190,7 +260,7 @@ private:
 		SDL_FRect background{ screenTopLeft.x, screenTopLeft.y, screenWidth, screenHeight };
 		SDL_SetRenderDrawColor(renderer, settings.bgColor.r, settings.bgColor.g, settings.bgColor.b, settings.bgColor.a);
 
-		SDL_RenderPresent(renderer);
+		// SDL_RenderPresent(renderer); --> Causes stuttering?
 
 	}
 
@@ -209,10 +279,6 @@ private:
 	//}
 
 };
-
-
-
-
 
 
 
